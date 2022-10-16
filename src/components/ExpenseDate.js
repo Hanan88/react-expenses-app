44c68fb484a15333f7340bad36bf6a8e6ce29e04
Hanan88 <Hanan.Ahmed.8@hotmail.com>
2022-10-16 17:17:19 +0200

    ExpenseApp

---
+++ src/components/ExpenseDate.js
@@ -0,0 +1,17 @@
+import "./ExpenseDate.css";
+
+function ExpenseDate(props) {
+  const month = props.date.toLocaleDateString("en-us", { month: "long" });
+  const year = props.date.toLocaleDateString("en-us", { year: "numeric" });
+  const day = props.date.toLocaleDateString("en-us", { day: "numeric" });
+  
+  return (
+    <div className="expense-date">
+      <div className="expense-date__month">{month}</div>
+      <div className="expense-date__year">{year}</div>
+      <div className="expense-date__day">{day}</div>
+    </div>
+  );
+}
+
+export default ExpenseDate;
